02ab48aca91bd5d5890e3cc078194fb58c2246fc
Solanoize <9783318+solanoize@users.noreply.github.com>
2025-05-22 21:20:05 +0700

    capek cuii

--- src/test/java/com/juaracoding/test/authentications/SignInWithoutPasswordTest.java
+++ src/test/java/com/juaracoding/test/authentications/SignInWithoutPasswordTest.java
@@ -7,24 +7,22 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.SkipException;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.juaracoding.test.DriverSingleton;
 
-public class SignInTest {
+public class SignInWithoutPasswordTest {
   WebDriver driver;
   WebElement usernameField;
-  WebElement passwordField;
   WebElement buttonLogin;
+
   List<String> missingElements = new ArrayList<>();
   boolean skipTest = false;
 
@@ -33,20 +31,17 @@ public class SignInTest {
       return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
     } catch (Exception e) {
       missingElements.add(name);
-      // skipTest = false;
       return null;
     }
   }
 
   @BeforeClass
   public void setup() throws InterruptedException {
-    Thread.sleep(3000);
     driver = DriverSingleton.createOrGetDriver();
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     usernameField = checkAndGet(wait, "usernameField", By.id("user-name"));
-    passwordField = checkAndGet(wait, "passwordField", By.id("password"));
     buttonLogin = checkAndGet(wait, "buttonLogin", By.id("login-button"));
 
     if (!missingElements.isEmpty()) {
@@ -64,24 +59,17 @@ public class SignInTest {
   @Test
   public void testStep01() throws InterruptedException {
     try {
-
       Thread.sleep(3000);
       usernameField.sendKeys("standard_user");
-      Thread.sleep(1000);
-      passwordField.sendKeys("secret_sauce");
-      Thread.sleep(1000);
+      Thread.sleep(2000);
       buttonLogin.click();
+      Thread.sleep(2000);
+      usernameField.clear();
+      Thread.sleep(3000);
 
-      String expected = "https://www.saucedemo.com/v1/inventory.html";
-      String actual = driver.getCurrentUrl();
-
-      System.out.println(actual);
-
-      Assert.assertEquals(actual, expected);
     } catch (Exception e) {
       // TODO: handle exception
       throw new SkipException("testStep01: " + e.getMessage());
     }
   }
-
 }
--- src/test/java/com/juaracoding/test/authentications/SignInWithoutUsernameTest.java
+++ src/test/java/com/juaracoding/test/authentications/SignInWithoutUsernameTest.java
@@ -5,58 +5,73 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.SkipException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.juaracoding.test.DriverSingleton;
 
-public class SignInWithoutCredential {
-  WebElement buttonLogin;
-  WebElement usernameField;
+public class SignInWithoutUsernameTest {
   WebDriver driver;
-  List<String> missingElements = new ArrayList<>();
-  boolean skipClass = false;
+  WebElement passwordField;
+  WebElement buttonLogin;
 
-  // Helper untuk cek dan catat elemen
+  List<String> missingElements = new ArrayList<>();
+  boolean skipTest = false;
+
   private WebElement checkAndGet(WebDriverWait wait, String name, By locator) {
     try {
       return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
-    } catch (TimeoutException e) {
+    } catch (Exception e) {
       missingElements.add(name);
+      // skipTest = false;
       return null;
     }
   }
 
   @BeforeClass
-  public void setup() {
+  public void setup() throws InterruptedException {
+    Thread.sleep(3000);
     driver = DriverSingleton.createOrGetDriver();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-    buttonLogin = checkAndGet(wait, "buttonLogin", By.xpath("/html/body/div[2]/div[1]/div/div/form/input[3]"));
-    usernameField = checkAndGet(wait, "usernameField", By.xpath("//*[@data-test=\"usernames\"]"));
+    passwordField = checkAndGet(wait, "passwordField", By.id("password"));
+    buttonLogin = checkAndGet(wait, "buttonLogin", By.id("login-button"));
 
     if (!missingElements.isEmpty()) {
-      skipClass = true;
+      skipTest = true;
     }
   }
 
   @BeforeMethod
   public void checkElementsExist() {
-    if (skipClass) {
+    if (skipTest) {
       throw new SkipException("No element found: " + String.join(", ", missingElements));
     }
   }
 
   @Test
-  public void testStep03() {
-    usernameField.sendKeys("hello");
-    buttonLogin.click();
+  public void testStep01() throws InterruptedException {
+    try {
+      Thread.sleep(2000);
+      passwordField.sendKeys("secret_sauce");
+      Thread.sleep(2000);
+      buttonLogin.click();
+      Thread.sleep(2000);
+      passwordField.clear();
+      Thread.sleep(2000);
+    } catch (Exception e) {
+      // TODO: handle exception
+      throw new SkipException("testStep01: " + e.getMessage());
+    }
   }
 }
